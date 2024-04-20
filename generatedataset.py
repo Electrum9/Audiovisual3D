14079e6b399e3bec477673d8913b3f71c02d3d60
jinghangli98 <jil202@pitt.edu>
2024-04-20 19:39:07 -0400

    fixed camera pos

--- generatedataset.py
+++ generatedataset.py
@@ -70,15 +70,15 @@ def get_random_camera(boundaries):
     # currently: chooses a random point in the middle of the air
     camera_loc = [choose_random(boundaries[0][0], boundaries[1][0]), (boundaries[0][1] + boundaries[1][1]) / 2, choose_random(boundaries[0][2], boundaries[1][2])]
     camera_loc = torch.tensor([val.item() for val in camera_loc]).unsqueeze(0)
+    camera_loc = -camera_loc
     azim = choose_random(-180.0, 180.0)
     elev = choose_random(-45.0, 45.0)
     direc = [math.cos(elev) * math.sin(azim), math.sin(elev), math.cos(elev) * math.cos(azim)]
-    pdb.set_trace()
     R = pytorch3d.renderer.look_at_rotation(camera_loc, direc)
 
     camera = pytorch3d.renderer.FoVPerspectiveCameras(
         R=R,
-        T=torch.inverse(R) @ camera_loc,
+        T=torch.inverse(R) @ camera_loc.unsqueeze(-1).squeeze(),
         device=device
     )
     return camera
@@ -141,11 +141,10 @@ if __name__ == "__main__":
         
         audio, origin, mic_loc, speaker_loc, boundaries, mesh = get_random_datapoint()
         audio, origin, mic_loc, speaker_loc, boundaries = convert_to_torch(audio, origin, mic_loc, speaker_loc, boundaries)
-
-
         boundaries = set_boundaries_buffer(boundaries, torch.tensor([0.1, 0.05, 0.1], device = device))
         lights = get_random_lighting(boundaries)
         camera = get_random_camera(boundaries)
+        pdb.set_trace()
         rgb = get_rgb(mesh, camera, renderer, lights)
         # rgb_aug = apply_augmentation(rgb)
         depth_map = get_depth_map(mesh, camera, rasterizer)
