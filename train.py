84c93c4aa776c228ef8d1f6c14f97bd18307ccbc
VB <vikram.bhagavatula@gmail.com>
2024-04-29 23:49:01 -0400

    gitignore zone identifier files

--- train.py
+++ train.py
@@ -31,6 +31,57 @@ def loss_reg(diff):
     # TODO: not sure of shape of grad
     return 0
 
+def gauss_kernel(size=5, device=torch.device('cpu'), channels=3):
+    kernel = torch.tensor([[1., 4., 6., 4., 1],
+                           [4., 16., 24., 16., 4.],
+                           [6., 24., 36., 24., 6.],
+                           [4., 16., 24., 16., 4.],
+                           [1., 4., 6., 4., 1.]])
+    kernel /= 256.
+    kernel = kernel.repeat(channels, 1, 1, 1)
+    kernel = kernel.to(device)
+    return kernel
+
+def downsample(x):
+    return x[:, :, ::2, ::2]
+
+def upsample(x):
+    cc = torch.cat([x, torch.zeros(x.shape[0], x.shape[1], x.shape[2], x.shape[3], device=x.device)], dim=3)
+    cc = cc.view(x.shape[0], x.shape[1], x.shape[2]*2, x.shape[3])
+    cc = cc.permute(0,1,3,2)
+    cc = torch.cat([cc, torch.zeros(x.shape[0], x.shape[1], x.shape[3], x.shape[2]*2, device=x.device)], dim=3)
+    cc = cc.view(x.shape[0], x.shape[1], x.shape[3]*2, x.shape[2]*2)
+    x_up = cc.permute(0,1,3,2)
+    return conv_gauss(x_up, 4*gauss_kernel(channels=x.shape[1], device=x.device))
+
+def conv_gauss(img, kernel):
+    img = torch.nn.functional.pad(img, (2, 2, 2, 2), mode='reflect')
+    out = torch.nn.functional.conv2d(img, kernel, groups=img.shape[1])
+    return out
+
+def laplacian_pyramid(img, kernel, max_levels=3):
+    current = img
+    pyr = []
+    for level in range(max_levels):
+        filtered = conv_gauss(current, kernel)
+        down = downsample(filtered)
+        up = upsample(down)
+        diff = current-up
+        pyr.append(diff)
+        current = down
+    return pyr
+
+class LapLoss(torch.nn.Module):
+    def __init__(self, max_levels=3, channels=1, device=torch.device('cpu')):
+        super(LapLoss, self).__init__()
+        self.max_levels = max_levels
+        self.gauss_kernel = gauss_kernel(channels=channels, device=device)
+        
+    def forward(self, input, target):
+        pyr_input  = laplacian_pyramid(img=input, kernel=self.gauss_kernel, max_levels=self.max_levels)
+        pyr_target = laplacian_pyramid(img=target, kernel=self.gauss_kernel, max_levels=self.max_levels)
+        return sum(torch.nn.functional.l1_loss(a, b) for a, b in zip(pyr_input, pyr_target))
+
 def loss_midas(disparity_pred, depth_gt):
     # input: pred and gt of shape (B, H, W), grad of shape ???
     # output: losses of shape (B,)
@@ -40,6 +91,7 @@ def loss_midas(disparity_pred, depth_gt):
     if torch.any(torch.isnan(disparity_pred)).item():
         breakpoint()
 
+    B, H, W = disparity_pred.shape
     disparity_pred_1d = disparity_pred.view(B, -1)
     disparity_gt_1d = 1 / (depth_gt.view(B, -1) + 1e-5) # make this disparity
 
@@ -54,7 +106,7 @@ def loss_midas(disparity_pred, depth_gt):
     diff = disparity_pred_1d - disparity_gt_1d
 
     loss = loss_sstrim(diff) 
-    reg = loss_reg(diff)
+    reg = LapLoss(max_levels=4, device=torch.device('cuda'))(disparity_pred_1d.reshape(B, 1, H, W), disparity_gt_1d.reshape(B, 1, H, W))
 
     total = torch.mean(loss + reg)
 
