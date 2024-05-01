4a218f0595ebf5bcf8e8526892862b134e434151
jinghangli98 <jil202@pitt.edu>
2024-05-01 00:37:12 -0400

    added working inference code for livingroom

--- inference.py
+++ inference.py
@@ -67,7 +67,25 @@ def collate_fn(batch):
     
     return stacked_rgb, stacked_audio, stacked_speakerloc, stacked_micloc, stacked_depth
 
+def saveinferenceImg(idx, model, rgb, audio, speaker_pos, mic_pos, depths_gt):
+        depths_pred, transforms = model(rgb, audio, speaker_pos, mic_pos)
+        scales = transforms[:, 0].unsqueeze(2).unsqueeze(3) # (B, 8, 1, 1)
+        translations = transforms[:, 1].unsqueeze(2).unsqueeze(3) # (B, 8, 1, 1)
+        depths_pred = depths_pred * scales + translations # (B, 8, 512, 512)
+        loss = scaled_depth_loss(depths_pred, depths_gt)
 
+        img = depths_pred.detach().cpu()
+        img_gt = depths_gt.detach().cpu()
+        rgb_img_gt = rgb.detach().cpu()
+        img = plt.get_cmap('viridis')((img[0,0] - img[0,0].min())/(img[0,0].max() - img[0,0].min()))
+        showimg = (rgb_img_gt[0,0,...] + abs((rgb_img_gt[0,0,...].min()))).unsqueeze(0)
+        showimg = F.interpolate(showimg, [512, 512], mode='bilinear', align_corners=False).squeeze().permute(1,2,0)
+        img_gt = plt.get_cmap('viridis')((img_gt[0,0] - img_gt[0,0].min())/(img_gt[0,0].max() - img_gt[0,0].min()))
+        output = torch.hstack((showimg/showimg.max(), torch.tensor(img_gt[...,:3]), torch.tensor(img[...,:3])))
+        # plt.imsave(f"rgb_gt_images/test_{idx}.png", output.numpy())
+        
+        return loss.cpu().item(), showimg/showimg.max(), torch.tensor(img_gt[...,:3]), torch.tensor(img[...,:3])
+    
 def inference_model(args):
     dataset = CustomImageDataset(args)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn)
@@ -79,8 +97,13 @@ def inference_model(args):
     
     train_loader = iter(dataloader)
     loss_vis = 0
+    loss_vis_noAudio = 0
+    pred_depths = []
+    pred_depth_noAudios = []
+    diffs = []
     for idx in range(len(train_loader)):
         rgb, audio, speaker_pos, mic_pos, depths_gt = next(train_loader)
+        
         
         rgb = rgb.to(args.device)
         audio = audio.to(args.device)
@@ -88,34 +111,31 @@ def inference_model(args):
         mic_pos = mic_pos.to(args.device)
         depths_gt = depths_gt.to(args.device)
 
+        
         with torch.no_grad():
-            depths_pred, transforms = model(rgb, audio, speaker_pos, mic_pos)
-            pdb.set_trace()
-            scales = transforms[:, 0].unsqueeze(2).unsqueeze(3) # (B, 8, 1, 1)
-            translations = transforms[:, 1].unsqueeze(2).unsqueeze(3) # (B, 8, 1, 1)
-            depths_pred = depths_pred * scales + translations # (B, 8, 512, 512)
-            
-            loss = scaled_depth_loss(depths_pred, depths_gt)
-            loss_vis += loss.cpu().item()
+            loss_epoch, rgb_save, depth_gt, pred_depth = saveinferenceImg(idx, model, rgb, audio, speaker_pos, mic_pos, depths_gt)
+            loss_epoch /= rgb.shape.numel()
+            loss_vis += loss_epoch
+            loss_epoch_noAudio, _, _, pred_depth_noAudio = saveinferenceImg(idx, model, rgb, torch.randn(audio.shape).float().to('cuda'), speaker_pos, mic_pos, depths_gt)
+            loss_epoch_noAudio /= rgb.shape.numel()
+            loss_vis_noAudio += loss_epoch_noAudio
+            diff = (pred_depth - pred_depth_noAudio).abs()
+            diff = diff/diff.max()
+            img_out = torch.hstack(((rgb_save*256).long(), (depth_gt*256).long(), (pred_depth*256).long(), (pred_depth_noAudio*256).long(), (diff*256).long()))
+            pred_depths.append(pred_depth)
+            pred_depth_noAudios.append(pred_depth_noAudio)
+            diffs.append(diff.sum())
+            plt.imsave(f"rgb_gt_images/test_{idx}.png", img_out.numpy().astype(np.uint8))
 
-            img = depths_pred.detach().cpu()
-            img_gt = depths_gt.detach().cpu()
-            rgb_img_gt = rgb.detach().cpu()
-            img = plt.get_cmap('viridis')((img[0,0] - img[0,0].min())/(img[0,0].max() - img[0,0].min()))
-            showimg = (rgb_img_gt[0,0,...] + abs((rgb_img_gt[0,0,...].min()))).unsqueeze(0)
-            showimg = F.interpolate(showimg, [512, 512], mode='bilinear', align_corners=False).squeeze().permute(1,2,0)
-            img_gt = plt.get_cmap('viridis')((img_gt[0,0] - img_gt[0,0].min())/(img_gt[0,0].max() - img_gt[0,0].min()))
-            output = torch.hstack((showimg/showimg.max(), torch.tensor(img_gt[...,:3]), torch.tensor(img[...,:3])))
-            plt.imsave(f"rgb_gt_images/test_{idx}.png", output.numpy())
-            
-    
-        
-        
+    average_loss = loss_vis/(idx+1)
+    average_loss_noAudio = loss_vis_noAudio/(idx+1)
+    print(f'{average_loss} | {average_loss_noAudio}')
+
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--batch_size", default=32, type=int)
+    parser.add_argument("--batch_size", default=2, type=int)
     parser.add_argument("--image_size", default=512, type=int)
     parser.add_argument('--device', default='cuda', type=str)
     parser.add_argument("--max_iter", default=100000, type=int)
